0b80bb7a1da9585c92f49df3e4e311793f4bd6ec
Junior Nuñes <Junior Nues@DESKTOP-S4DNI64>
2024-08-09 10:55:05 -0600

    Aqui esta el codigo completo de los 4 apartados hechos ya, el primero de es de retornar la palabra ingresada al reves, el segundo determina si un numero es numero perfecto o no, el tercero determina si es un numero primo, y el ultimo es un contador de votaciones de un pais que determina el ganador dependiendo de cual de las votaciones es mayor al 60%

--- NetBeansProjects/Nuñez_Junior_Prueba1Lab/src/nuñez_junior_prueba1lab/Nuñez_Junior_Prueba1Lab.java
+++ NetBeansProjects/Nuñez_Junior_Prueba1Lab/src/nuñez_junior_prueba1lab/Nuñez_Junior_Prueba1Lab.java
@@ -12,9 +12,19 @@ import java.util.Scanner;
  */
 public class Nuñez_Junior_Prueba1Lab {
 
+   
+
     public static void main(String[] args) {
         Scanner lea = new Scanner(System.in);
-        Random random=new Random();
+        Random random = new Random();
+        
+        int contpalreves=0;
+        int contnumperfecto=0;
+        int contnumprimo=0;
+        int contvotaciones=0;
+        
+        
+        
 
         while (true) {
             System.out.println("\n Ingrese que desea hacer: " +
@@ -24,43 +34,121 @@ public class Nuñez_Junior_Prueba1Lab {
                     "\n4. Votaciones" +
                     "\n5. Salir");
             int siguiente = lea.nextInt();
-           
+
             switch (siguiente) {
                 case 1:
+                    contpalreves++;
                     System.out.println("Ingrese una palabra");
                     String palabrita = lea.next();
-                    
+
                     for (int i = palabrita.length() - 1; i >= 0; i--) {
                         System.out.print(palabrita.charAt(i));
                     }
-                   
+                    System.out.println(); 
                     break;
-                    
+
                 case 2:
-                    int num=0;
-                    int suma=0;
+                    contnumperfecto++;
                     System.out.println("Favor ingrese el numero al que desea evaluar: ");
-                            num = lea.nextInt();
-                            for (int cont = 1; cont< num; cont++) {
-                                if (num % cont == 0) {
-                                    suma = suma + cont;
-                                }
-                            }
-                            if ( suma == num) {
-                                System.out.println("El numero es perfecto");
-                                break;
-                            } else {
-                                System.out.println("El numero no es perfecto.");
-                                break;
-                            }
-                            
+                    int num = lea.nextInt();
+                    int suma = 0;
+
+                    for (int cont = 1; cont < num; cont++) {
+                        if (num % cont == 0) {
+                            suma += cont;
+                        }
+                    }
+
+                    if (suma == num) {
+                        System.out.println("El numero es perfecto");
+                    } else {
+                        System.out.println("El numero no es perfecto.");
+                    }
+                    break;
+
                 case 3:
-                    
-                    
+                    contnumprimo++;
+                    int bot = random.nextInt(100);
+                    System.out.println("Número generado: " + bot);
+
+                    int contador = 1;
+                    int primo = 0;
+
+                    while (contador <= bot) {
+                        if (bot % contador == 0) {
+                            primo += contador;
+                        }
+                        contador++;
+                    }
+
+                    if (primo == bot + 1) {
+                        System.out.println("Es primo");
+                        System.out.println("Sus únicos divisores son: 1 y " + bot);
+                    } else {
+                        System.out.println("No es primo");
+                    }
+                    break;
+                 
+                case 4:
+                    contvotaciones++;
+                    int votoazul = 0;
+                    int votorojo = 0;
+                    int votonegro = 0;
+                    int votoamarillo = 0;
+                    int votonulo = 0;
+
+                    System.out.println("Ingrese cuantos votantes hay en el país: ");
+                    int cantvotantes = lea.nextInt();
+
+                    for (int cont = 0; cont < cantvotantes; cont++) {
+                        System.out.println("\n TIPO DE VOTOS" +
+                                "\n1. VOTO AZUL" +
+                                "\n2. VOTO ROJO" +
+                                "\n3. VOTO NEGRO" +
+                                "\n4. VOTO AMARILLO");
+                        System.out.println("Ingrese su tipo de voto: ");
+                        int votos = lea.nextInt();
+
+                        if (votos != 1 && votos != 2 && votos != 3 && votos != 4) {
+                            System.out.println("VOTO NULO");
+                            votonulo++;
+                        } else if (votos == 1) {
+                            votoazul++;
+                        } else if (votos == 2) {
+                            votorojo++;
+                        } else if (votos == 3) {
+                            votonegro++;
+                        } else {
+                            votoamarillo++;
+                        }
+                    }
+
+                    int totalvotos = votoazul + votorojo + votonegro + votoamarillo + votonulo;
+                    int votosvalidos = totalvotos - votonulo;
+
+                    if (votosvalidos >= 0.6 * totalvotos) {
+                        if (votoazul > votorojo && votoazul > votonegro && votoazul > votoamarillo) {
+                            System.out.println("La planilla ganadora es AZUL.");
+                        } else if (votorojo > votoazul && votorojo > votonegro && votorojo > votoamarillo) {
+                            System.out.println("La planilla ganadora es ROJA.");
+                        } else if (votonegro > votoazul && votonegro > votorojo && votonegro > votoamarillo) {
+                            System.out.println("La planilla ganadora es NEGRA.");
+                        } else {
+                            System.out.println("La planilla ganadora es AMARILLA.");
+                        }
+                    } else {
+                        System.out.println("VOTACION FALLIDA");
+                    }
+                    break;
 
                 case 5:
                     System.out.println("Saliendo del programa...");
-                   
+                    System.out.println("Numero de ingresos a Palabra alreves: "+ contpalreves);
+                    System.out.println("Numero de ingresos a Numero Perfecto: "+contnumperfecto);
+                    System.out.println("Numero de ingresos a Primos: "+contnumprimo);
+                    System.out.println("Numero de ingresos a Votaciones: "+ contvotaciones);
+                    
+                    return; 
 
                 default:
                     System.out.println("Opción no válida. Intente de nuevo.");
