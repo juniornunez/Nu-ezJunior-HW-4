e9a7cf5cc2f776d3238c491a8284895c1cc9dbaa
Junior Nuñes <Junior Nues@DESKTOP-S4DNI64>
2024-10-25 16:23:05 -0600

    Aqui esta el juego de sudoku funcionable con las opciones y funciones que se piden

---
+++ NetBeansProjects/ Andru/src/andru/Andru.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package andru;
+
+/**
+ *
+ * @author Junior Nuñes
+ */
+public class Andru {
+protected String nombre;
+protected String mascota;
+protected int edad;
+
+    public Andru(String nombre, String mascota, int edad){
+        this.nombre=nombre;
+        this.mascota=mascota;
+        this.edad=edad;
+       
+    }
+    public void MostrarDatos(){
+        System.out.println("Me llaman el papa de Angel "+ nombre);
+        System.out.println("Mi perro se llama "+ );
+    }
+}
